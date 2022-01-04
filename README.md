732bfcaef3dc1216b57749b952db58032346976d
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-04 21:15:01 +0530

    Keyword & Readme or some typo fix

---
+++ README.md
@@ -0,0 +1,15 @@
+# `npx suryapratap`
+
+> Get to know Suryapratap Singh Suryavanshi using your command line with Node.Js
+
+## Usage 💻
+
+To use this CLI run the following command:
+
+```sh
+npx suryapratap
+```
+
+<hr>
+
+Thank You :)
--- index.js
+++ index.js
@@ -21,7 +21,8 @@ Suryapratap Singh Suryavanshi
 
 I have been interested in computers since childhood. I always wanted to understand it, I understood it, I found it very interesting, then I thought of making it my work out of my interest and just pursuing that desire. I have also created a lot of websites and software out of my jumping interest which was really the solution to some real-world problems. I love solving real-world problems with my code. like my recent project a web app that bypasses Instagram proxy and downloads user display pictures.
 
-📖 GitHub Profile: https://github.com/suryapratapsinghsuryavanshi
-💻 Website: http://suryapratap.netlify.app
-😎 Blog: https://auth.geeksforgeeks.org/user/mrsuryapratap/articles
+😎 GitHub:    https://github.com/suryapratapsinghsuryavanshi
+💻 Portfolio: http://suryapratap.netlify.app
+📖 Blog:      https://auth.geeksforgeeks.org/user/mrsuryapratap/articles
+📭 Email:     suryprtaps@gmail.com
 `);
