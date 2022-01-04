0c5fb9294d8af092d86cc53095cfb6854c254bd6
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-04 22:40:06 +0530

    add colors

--- index.js
+++ index.js
@@ -2,12 +2,19 @@
 const { clearLog } = require("./utils-info");
 const pkgJSON = require('./package.json');
 const welcome = require("cli-welcome");
+const chalk = require("chalk");
+const dim = chalk.dim;
+const italic = chalk.italic;
+const portfoliClr = chalk.bgHex(`#b3dcff`).hex(`#000000`).bold;
+const githubClr = chalk.bgHex(`#6cc644`).hex(`#000000`).bold;
+const emailClr = chalk.bgHex(`#3be8b0`).hex(`#000000`).bold;
+const blogClr = chalk.bgHex(`#f57d00`).hex(`#000000`).bold;
 
 // Clear the console
 clearLog();
 
 welcome({
-    title: pkgJSON.name,
+    title: `Suryapratap Singh Suryavanshi`,
     tagLine: 'Hey, nice to meet you!',
     description: pkgJSON.description,
     version: pkgJSON.version,
@@ -16,13 +23,12 @@ welcome({
 	bold: true
 });
 
-console.log(`
-Suryapratap Singh Suryavanshi
+console.log(`${italic(
+    `I have been interested in computers since childhood. I always wanted to understand it, I understood it, I found it very interesting, then I thought of making it my work out of my interest and just pursuing that desire. I have also created a lot of websites and software out of my jumping interest which was really the solution to some real-world problems. I love solving real-world problems with my code. like my recent project a web app that bypasses Instagram proxy and downloads user display pictures.`
+)}
 
-I have been interested in computers since childhood. I always wanted to understand it, I understood it, I found it very interesting, then I thought of making it my work out of my interest and just pursuing that desire. I have also created a lot of websites and software out of my jumping interest which was really the solution to some real-world problems. I love solving real-world problems with my code. like my recent project a web app that bypasses Instagram proxy and downloads user display pictures.
-
-😎 GitHub:    https://github.com/suryapratapsinghsuryavanshi
-💻 Portfolio: http://suryapratap.netlify.app
-📖 Blog:      https://auth.geeksforgeeks.org/user/mrsuryapratap/articles
-📭 Email:     suryprtaps@gmail.com
+${portfoliClr(` Portfolio `)} ${dim(`http://suryapratap.netlify.app`)}
+${githubClr(` GitHub `)}    ${dim(`https://github.com/suryapratapsinghsuryavanshi`)}
+${emailClr(` Email `)}     ${dim(`suryprtaps@gmail.com`)}
+${blogClr(` Blog `)}      ${dim(`https://auth.geeksforgeeks.org/user/mrsuryapratap/articles`)}
 `);
