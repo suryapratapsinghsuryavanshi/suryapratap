71dade48cac5d691a6a91f5f61cef3ffbeedc395
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-05 21:15:55 +0530

    fix welcome-cli

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const { clearLog } = require("./utils-info");
 const pkgJSON = require('./package.json');
-const welcome = require("cli-welcome");
 const chalk = require("chalk");
 const dim = chalk.dim;
 const italic = chalk.italic;
@@ -10,18 +9,28 @@ const githubClr = chalk.bgHex(`#6cc644`).hex(`#000000`).bold;
 const emailClr = chalk.bgHex(`#3be8b0`).hex(`#000000`).bold;
 const blogClr = chalk.bgHex(`#f57d00`).hex(`#000000`).bold;
 
+// Alerts.
+const success = chalk.green;
+const info = chalk.blue;
+const warning = chalk.keyword('orange');
+const error = chalk.red;
+
 // Clear the console
 clearLog();
 
-welcome({
-    title: `Suryapratap Singh Suryavanshi`,
+const cli = {
+    title: ` Suryapratap Singh Suryavanshi `,
     tagLine: 'Hey, nice to meet you!',
     description: pkgJSON.description,
     version: pkgJSON.version,
     bgColor: `#FADC00`,
 	color: `#000000`,
 	bold: true
-});
+}
+
+console.log(`
+${chalk.bgHex(cli.bgColor).hex(cli.color)(cli.title)} ${ "v" + cli.version} ${chalk.dim(cli.tagLine)}\n${chalk.dim(cli.description)}
+`);
 
 console.log(`${italic(
     `I have been interested in computers since childhood. I always wanted to understand it, I understood it, I found it very interesting, then I thought of making it my work out of my interest and just pursuing that desire. I have also created a lot of websites and software out of my jumping interest which was really the solution to some real-world problems. I love solving real-world problems with my code. like my recent project a web app that bypasses Instagram proxy and downloads user display pictures.`
