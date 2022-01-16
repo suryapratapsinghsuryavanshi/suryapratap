0bd9c901c067b06d55e3a49f8b51196d50770221
Suryapratap Singh <suryprtaps@gmail.com>
2022-01-16 17:16:38 +0530

    added meow for cli arguments

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 #!/usr/bin/env node
 
-let {bio, social } = require("./utils/data");
+let { bio, social } = require("./utils/data");
+let meow = require("meow");
+
+let helperText = `Suryapratap CLI`;
+let cli = meow(helperText, {
+    flags: {
+        social: {
+            type: 'boolean',
+            default: true
+        },
+        bio: {
+            type: 'boolean',
+            default: true
+        }
+    }
+});
 
 (() => {
     require("./utils/init")();
-    console.log(bio);
-    console.log(social);
+    cli.flags.bio ? console.log(bio) : '';
+    cli.flags.social ? console.log(social) : '';
 })();
